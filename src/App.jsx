95235340dcfcee469e707f0a45a09d15665112cd
Salvatore Filippone <sfilippone@gmail.com>
2025-07-03 13:42:52 -0700

    working on tabs

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
 import { CORE_CONCEPTS } from "./data.js";
-import Header from "./components/header.jsx";
+import Header from "./components/Header/Header.jsx";
 import CoreConcept from "./components/CoreConcept.jsx";
-
+import TabButton from "./components/TabButton.jsx";
 
 function App() {
+  function handleSelect(selectedButton) {
+    // selectedButton => 'components', 'jsx', 'props', 'state' 
+    console.log(selectedButton);
+  }
+
   return (
     <div>
       <Header />
@@ -15,9 +20,17 @@ function App() {
              <CoreConcept {...CORE_CONCEPTS[1]} />
              <CoreConcept {...CORE_CONCEPTS[2]} />
              <CoreConcept {...CORE_CONCEPTS[3]} />
-          
-            
           </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
+            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
+          </menu>
+          Dynamic Content
         </section>
       </main>
     </div>
